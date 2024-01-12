99748426ee2ad9b5250514a5b959d19603889d39
FlavioRoman <flavio.roman@genz.com.py>
2024-01-12 16:21:41 -0300

    update

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero/main";
 import About from "../components/About/main";
 import Navbar from "../components/Navbar/main";
 import Footer from "../components/Footer/main";
-// import Dealers from "../components/Dealers/main";
+import Dealers from "../components/Dealers/main";
 import Products from "../components/Products/main";
 import TopButton from "../components/TopButton/main";
 import Testimonials from "../components/Testimonials/main";
@@ -22,7 +22,7 @@ export default function Main() {
       <Hero />
       <About />
       <Products />
-      {/* <Dealers /> */}
+      <Dealers />
       <Testimonials />
       {/* <Authentication /> */}
       <Footer />
