fc956a785843220da8a602b84d7282d1b5701db2
FlavioRoman <flavio.roman@genz.com.py>
2024-01-12 13:21:51 -0300

    update

--- src/components/Testimonials/main.jsx
+++ src/components/Testimonials/main.jsx
@@ -1,37 +1,75 @@
-// STYLE
+// // STYLE
 import styles from "./main.module.css";
 
-// SLIDE
-import { Slide } from "react-slideshow-image";
+// SWIPER STYLES
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-// CONTANSTS VALUES
-import { TESTIMONIALS } from "../../constant/main";
+// IMAGES
+import img01 from "../../assets/img01.webp";
+import img02 from "../../assets/img02.webp";
+import img03 from "../../assets/img03.webp";
 
-import "react-slideshow-image/dist/styles.css";
+// SWIPER REACT
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// REQUIRED MODULES
+import { Navigation, Pagination } from "swiper/modules";
 
 export default function Testimonials() {
   return (
-    <>
-      <main id="testimonials" className={styles.container}>
-        {/* <h1 className={styles.title}>What a customer say.</h1> */}
-        <Slide>
-          {TESTIMONIALS.map((fadeImage, index) => (
-            <div key={fadeImage.caption} className={styles.container_slide}>
-              <div key={index} className={styles.slide}>
-                <div className={styles.content}>
-                  <h3 className={styles.name}>Name</h3>
-                  <p className={styles.desc}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Autem placeat in laborum ipsam totam id aut sunt porro quo
-                    maxime.
-                  </p>
-                </div>
-                <img alt="image" src={fadeImage.url} className={styles.image} />
-              </div>
+    <main id="testimonials" className={styles.container}>
+      <Swiper
+        loop={true}
+        className="mySwiper"
+        navigation={true}
+        spaceBetween={30}
+        slidesPerView={1}
+        modules={[Pagination, Navigation]}
+        pagination={{
+          clickable: true,
+        }}
+      >
+        <SwiperSlide>
+          <div className={styles.content}>
+            <div>
+              <h3 className={styles.name}>Name</h3>
+              <p className={styles.desc}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Debitis, labore!
+              </p>
             </div>
-          ))}
-        </Slide>
-      </main>
-    </>
+            <img src={img01} className={styles.image} alt="image" />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className={styles.content}>
+            <div>
+              <h3 className={styles.name}>Name</h3>
+              <p className={styles.desc}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Debitis, labore!
+              </p>
+            </div>
+            <img src={img02} className={styles.image} alt="image" />
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className={styles.content}>
+            <div>
+              <h3 className={styles.name}>Name</h3>
+              <p className={styles.desc}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Debitis, labore!
+              </p>
+            </div>
+            <img src={img03} className={styles.image} alt="image" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </main>
   );
 }
